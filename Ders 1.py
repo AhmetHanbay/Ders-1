b4d952169241d6c0bc55e1a4f382e9c8a96d5b9b
AhmetHanbay <90829959+AhmetHanbay@users.noreply.github.com>
2021-10-27 14:06:14 +0300

    Ders 1 Yüklendi

---
+++ Ders 1.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+print("Merhaba Dünya!")
+
+
+# In[2]:
+
+
+x = input("Adınızı Giriniz = ")
+
+
+# In[3]:
+
+
+print(x)
+
+
+# In[8]:
+
+
+x = 10.65
+y = round(x);
+z = round(x,1);
+print(x)
+print(y)
+print(z)
+
+
+# In[9]:
+
+
+str = "HELLO WORLD!"
+print(str[0:5])
+
+
+# In[11]:
+
+
+str = "HELLO WORLD!"
+print(str * 2)
+
+
+# In[20]:
+
+
+str = "HELLO WORLD!"
+print(str+"Ahmet Fevzi")
+
+
+# In[1]:
+
+
+tuple = ("abcd","123,2.23,john")
+print(tuple)
+
